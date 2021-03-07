8f1a8621a3ad1f382314a19a6fb49d454b2a5dc0
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-07 17:21:21 -0600

    fail test for mage and knight stats
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -14,11 +14,26 @@ describe ('Character', () => {
 		expect(knight.hp).toEqual(80);
 		expect(knight.mp).toEqual(50);
 	}); 
+	
+	test ('should create a knight character with the correct stats values (Strength of 200, Defense of 100, Agility of 5, Intelligence of 5)', () => {
+		expect(knight.strength).toEqual(200);
+		expect(knight.defense).toEqual(100);
+		expect(knight.agility).toEqual(5);
+		expect(knight.intelligence).toEqual(5);
+	});
 
 	test ('should correctly create a character object for a mage with different hp 30 and mp 150 vlaues', () => {
 		expect(mage.hp).toEqual(30);
 		expect(mage.mp).toEqual(150);
 	});
+
+	test ('should create a mage character with the correct stats values (Strength of 40, Defense of 20, Agility of 70, Intelligence of 200)', () => {
+		expect(mage.strength).toEqual(40);
+		expect(mage.defense).toEqual(20);
+		expect(mage.agility).toEqual(70);
+		expect(mage.intelligence).toEqual(200);
+	});
+
 
 	test ('should correctly create a character object for a thief with different hp 30 and mp 150 vlaues', () => {
 		expect(thief.hp).toEqual(50);
