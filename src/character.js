0e79e60474a392ef36a6b8ae60897c1f679033ff
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-07 17:23:14 -0600

    pass test for mage and knight stats
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- src/character.js
+++ src/character.js
@@ -2,9 +2,17 @@ export default function Character (characterType) {
 	if (characterType === "knight") {
 		this.hp = 80;
 		this.mp = 50;
+		this.strength = 200;
+		this.defense = 100;
+		this.agility = 5;
+		this.intelligence = 5;
 	} else if (characterType === "mage") {
 		this.hp = 30;
 		this.mp = 150;
+		this.strength = 40;
+		this.defense = 20;
+		this.agility = 70;
+		this.intelligence = 200;
 	} else if (characterType === "thief") {
 		this.hp = 50;
 		this.mp = 50;
