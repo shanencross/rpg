c8ec7400946793ca090537f9eed0fdc32f9b75c1
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-07 16:47:20 -0600

    pass first test
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- src/character.js
+++ src/character.js
@@ -1,3 +1,12 @@
 export default function Character (characterType) {
-
+	if (characterType === "knight") {
+		this.hp = 80;
+		this.mp = 50;
+	} else if (characterType === "mage") {
+		this.hp = 30;
+		this.mp = 150;
+	} else if (characterType === "thief") {
+		this.hp = 50;
+		this.mp = 50;
+	}
 }
