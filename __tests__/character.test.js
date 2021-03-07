42997c7312806ae4cf838780789f10bac0b1da5f
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-03-07 17:16:32 -0600

    add beforeEach to test knight,  mage and thief
    Co-authored-by: Shanen Cross <shanencross@yahoo.com>

--- __tests__/character.test.js
+++ __tests__/character.test.js
@@ -1,29 +1,34 @@
 import Character from './../src/character.js'; 
 
 describe ('Character', () => {
+	let knight;
+	let mage;
+	let thief;
+	beforeEach(() => {
+		knight = new Character("knight");
+	  mage = new Character("mage");
+	  thief = new Character("thief");
+	});
+
   test ('should correctly create a character object for a knight with different hp 80 and mp 50 vlaues', () => {
-		const character = new Character ("knight");
-		expect(character.hp).toEqual(80);
-		expect(character.mp).toEqual(50);
+		expect(knight.hp).toEqual(80);
+		expect(knight.mp).toEqual(50);
 	}); 
 
 	test ('should correctly create a character object for a mage with different hp 30 and mp 150 vlaues', () => {
-		const character = new Character ("mage");
-		expect(character.hp).toEqual(30);
-		expect(character.mp).toEqual(150);
-	}); 
+		expect(mage.hp).toEqual(30);
+		expect(mage.mp).toEqual(150);
+	});
 
 	test ('should correctly create a character object for a thief with different hp 30 and mp 150 vlaues', () => {
-		const character = new Character ("thief");
-		expect(character.hp).toEqual(50);
-		expect(character.mp).toEqual(50);
+		expect(thief.hp).toEqual(50);
+		expect(thief.mp).toEqual(50);
 	});
 
 	test ('should create a thief character with the correct stats values (Strength of 10, Defense of 15, Agility of 100, Intelligence of 50)', () => {
-		const character = new Character("thief");
-		expect(character.strength).toEqual(10);
-		expect(character.defense).toEqual(15);
-		expect(character.agility).toEqual(100);
-		expect(character.intelligence).toEqual(50);
+		expect(thief.strength).toEqual(10);
+		expect(thief.defense).toEqual(15);
+		expect(thief.agility).toEqual(100);
+		expect(thief.intelligence).toEqual(50);
 	});
 });
