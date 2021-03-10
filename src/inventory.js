dd5cc7266fdc6367bd033ac71ad200467c6537c6
Shanen Cross <shanencross@yahoo.com>
2021-03-09 21:05:04 -0800

    Test 2 for addItem passes
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

--- src/inventory.js
+++ src/inventory.js
@@ -2,6 +2,12 @@ export default function Inventory() {
 	this.items = new Map();
 }
 
-Inventory.prototype.addItem = function() {
-	this.items.set("sword",1);
+Inventory.prototype.addItem = function(item) {
+	if (this.items.has(item)) {
+		let currentItemQuantity = this.items.get(item);
+		this.items.set(item, currentItemQuantity + 1);
+	}
+	else {
+		this.items.set(item, 1);
+	}
 };
