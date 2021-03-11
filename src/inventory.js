7f64f86a66769d1ea9d6e60434234f91e26827c0
Shanen Cross <shanencross@yahoo.com>
2021-03-10 21:07:48 -0800

    Update constructors to use ES6 classes, and all tests still pass
    Co-authored-by: Sofia Acosta <sofiaacostarascon@gmail.com>
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

--- src/inventory.js
+++ src/inventory.js
@@ -1,13 +1,15 @@
-export default function Inventory() {
-	this.items = new Map();
-}
+export default class Inventory {
+	constructor() {
+		this.items = new Map();
+	}
 
-Inventory.prototype.addItem = function(item) {
-	if (this.items.has(item)) {
-		let currentItemQuantity = this.items.get(item);
-		this.items.set(item, currentItemQuantity + 1);
-	}
-	else {
-		this.items.set(item, 1);
-	}
-};
+	addItem(item) {
+		if (this.items.has(item)) {
+			let currentItemQuantity = this.items.get(item);
+			this.items.set(item, currentItemQuantity + 1);
+		}
+		else {
+			this.items.set(item, 1);
+		}
+	};
+}
