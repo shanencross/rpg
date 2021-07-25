7f0b8205d57832045255604171020f0e2d57cb42
Shanen Cross <shanencross@yahoo.com>
2021-07-25 16:23:23 -0700

    add function for storing state
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>

---
+++ src/stateFunctions/storeState.js
@@ -0,0 +1,10 @@
+const storeState = (initialState) => {
+  let currentState = initialState;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = {...newState};
+    return newState;
+  }
+}
+
+export default storeState;
