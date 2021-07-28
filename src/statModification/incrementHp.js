ed8df7920d583b2f33da8aab75a506666b8ad198
Shanen Cross <shanencross@yahoo.com>
2021-07-27 20:34:39 -0700

    Remove state controlling functions to focus on composition
    Co-authored-by: HR Williams <halrubinwilliams@gmail.com>
    Co-authored-by: Juan Hasbun <zemenarwow@gmail.com>

--- src/statModification/incrementHp.js
+++
@@ -1,5 +0,0 @@
-import addToState from './../stateFunctions/addToState.js';
-
-const setHp = () => addToState("hp");
-
-export default setHp;
--- src/statModification/setHp.js
+++
@@ -1,3 +0,0 @@
-const setHp = () => changeState("hp");
-
-export default setHp;
